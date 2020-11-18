da78c7c000279a489bc31c9750b6fe401a2a27c4
Gabriel Ferreira <hi.gabrielferreira@gmail.com>
2020-11-17 23:08:25 -0300

    chore: eslint/prettier/ts config

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home: React.FC = () => (
+  <div>
+    <Head>
+      <title>NFe - Lite</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <main>
+      <h1>Lite</h1>
+    </main>
+  </div>
+)
+
+export default Home
