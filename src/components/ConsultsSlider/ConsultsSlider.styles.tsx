47c14b15ca28d81110794f9b1b045d110de50e97
Gabriel Ferreira <hi.gabrielferreira@gmail.com>
2020-11-21 03:12:39 -0300

    feature: general adjustments

--- src/components/ConsultsSlider/ConsultsSlider.styles.tsx
+++ src/components/ConsultsSlider/ConsultsSlider.styles.tsx
@@ -1,6 +1,30 @@
-import styled from 'styled-components'
+import styled, { CSSProperties, DefaultTheme } from 'styled-components'
 import RcSlider from 'rc-slider'
 import 'rc-slider/assets/index.css'
+
+const HANDLE_SIZE = 15 // px
+const BAR_WIDTH = 3 // px
+
+export const getHandleStyle = (theme: DefaultTheme): CSSProperties => ({
+  borderColor: theme.colors.primary,
+  height: HANDLE_SIZE,
+  width: HANDLE_SIZE,
+  marginLeft: -6,
+  backgroundColor: theme.colors.primary,
+  cursor: 'grabbing',
+})
+
+export const getTrackStyles = (theme: DefaultTheme): CSSProperties => ({
+  backgroundColor: theme.colors.primary,
+  width: BAR_WIDTH,
+  borderRadius: 2,
+})
+
+export const getRailStyles = (theme: DefaultTheme): CSSProperties => ({
+  backgroundColor: theme.colors.grey200,
+  width: BAR_WIDTH,
+  borderRadius: 2,
+})
 
 export const Slider = styled(RcSlider)`
   position: absolute;
